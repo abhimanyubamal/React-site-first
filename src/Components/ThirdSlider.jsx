d09315a6cd9ab53bf5e31f4a5596e004e12db9aa
abamal <abamal@uoguelph.ca>
2024-01-17 12:30:19 +0530

    Few modifications

--- src/Components/ThirdSlider.jsx
+++ src/Components/ThirdSlider.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Slider from "react-slick";
-import Slider1 from "../assets/slider1.png";
+import Slider1 from "../assets/Vector.png";
 import Slider2 from "../assets/slider2.png";
 import Slider3 from "../assets/slider3.png";
 import Slider4 from "../assets/slider4.png";
@@ -18,28 +18,35 @@ const ThirdSlider = () => {
     };
 
   return (
-    <div className='mt-[109px] max-w-[1321px] mx-auto'>
+    <div className='mt-[109px] max-w-[1321px] mx-auto third pb-[109px]'>
       <div className='flex items-center gap-[11px]'>
                         <div className='h-px w-[95px] bg-black'></div>
                         <h4 className='text-page-yellow font-pop text-lg '>Customer Says</h4>
           </div>
           <h2 className='text-text font-pop text-[35px] leading-normal'>Testimonials</h2>
           <Slider {...settings}>
-              <img src={Slider1} alt="slider images" className='w-[108px] h-[132px] rotate-[4.609deg] shadow-[0px_50px_50px_rgba(195,176,169,0.30)]' />
-              <img src={Slider2} alt="slider images" className='w-[115,3134px] h-119.489px] rotate-[29.257deg] shadow-[0px_50px_50px_rgba(118,68,35,0.30)]' />
-              <img src={Slider3} alt="slider images" className='w-[76px] h-[77px] rotate-[4.609deg] shadow-[0px_50px_50px_rgba(49,173,234,0.30)]' />
-              <img src={Slider4} alt="slider images" className='w-[116px] h-[134px] shadow-[0px_50px_50px_rgba(0,0,0,0.04)]' />
-              <img src={Slider5} alt="slider images" className='w-[92px] h-[129px] shadow-[0px_50px_50px_rgba(131,121,84,0.30)]' />
-              <img src={Slider6} alt="slider images" className='w-[83px] h-[102px] shadow-[0px_50px_50px_rgba(43,43,43,0.30)]' />
-              <img src={Slider7} alt="slider images" className='w-[116px] h-[136px] shadown-[0px_50px_50px_rgba(110,52,37,0.30)]' />
-              <img src={Slider1} alt="slider images" className='w-[108px] h-[132px] rotate-[4.609deg] shadow-[0px_50px_50px_rgba(195,176,169,0.30)]' />
-              <img src={Slider2} alt="slider images" className='w-[115,3134px] h-119.489px] rotate-[29.257deg] shadow-[0px_50px_50px_rgba(118,68,35,0.30)]' />
-              <img src={Slider3} alt="slider images" className='w-[76px] h-[77px] rotate-[4.609deg] shadow-[0px_50px_50px_rgba(49,173,234,0.30)]' />
-              <img src={Slider4} alt="slider images" className='w-[116px] h-[134px] shadow-[0px_50px_50px_rgba(0,0,0,0.04)]' />
-              <img src={Slider5} alt="slider images" className='w-[92px] h-[129px] shadow-[0px_50px_50px_rgba(131,121,84,0.30)]' />
-              <img src={Slider6} alt="slider images" className='w-[83px] h-[102px] shadow-[0px_50px_50px_rgba(43,43,43,0.30)]' />
-              <img src={Slider7} alt="slider images" className='w-[116px] h-[136px] shadown-[0px_50px_50px_rgba(110,52,37,0.30)]' />
-          </Slider>
+              <img src={Slider1} alt="slider images" className='rouned-full rotate-[4.609deg] drop-shadow-[0px_50px_50px_rgba(195,176,169,0.30)]' />
+              <img src={Slider2} alt="slider images" className='rouned-full rotate-[29.257deg] drop-shadow-[0px_50px_50px_rgba(118,68,35,0.30)]' />
+              <img src={Slider3} alt="slider images" className='rouned-full rotate-[4.609deg] drop-shadow-[0px_50px_50px_rgba(49,173,234,0.30)]' />
+              <img src={Slider4} alt="slider images" className='rouned-full drop-shadow-[0px_50px_50px_rgba(0,0,0,0.04)]' />
+              <img src={Slider5} alt="slider images" className='rouned-full drop-shadow-[0px_50px_50px_rgba(131,121,84,0.30)]' />
+              <img src={Slider6} alt="slider images" className='rouned-full drop-shadow-[0px_50px_50px_rgba(43,43,43,0.30)]' />
+              <img src={Slider7} alt="slider images" className='rouned-full drop-shadow-[0px_50px_50px_rgba(110,52,37,0.30)]' />
+              <img src={Slider1} alt="slider images" className='rouned-full rotate-[4.609deg] drop-shadow-[0px_50px_50px_rgba(195,176,169,0.30)]' />
+              <img src={Slider2} alt="slider images" className='rouned-full rotate-[29.257deg] drop-shadow-[0px_50px_50px_rgba(118,68,35,0.30)]' />
+              <img src={Slider3} alt="slider images" className='rouned-full rotate-[4.609deg] drop-shadow-[0px_50px_50px_rgba(49,173,234,0.30)]' />
+              <img src={Slider4} alt="slider images" className='rouned-full drop-shadow-[0px_50px_50px_rgba(0,0,0,0.04)]' />
+              <img src={Slider5} alt="slider images" className='rouned-full drop-shadow-[0px_50px_50px_rgba(131,121,84,0.30)]' />
+              <img src={Slider6} alt="slider images" className='rouned-full drop-shadow-[0px_50px_50px_rgba(43,43,43,0.30)]' />
+              <img src={Slider7} alt="slider images" className='rouned-full drop-shadow-[0px_50px_50px_rgba(110,52,37,0.30)]' />
+      </Slider>
+      <div className='flex justify-center mt-4'>
+        <div className='w-[350px]'>
+          <h3 className='text-center font-pop text-lg leading-normal font-semibold'>Treepy Shoti</h3>
+          <h4 className='text-center font-pop text-sm leading-normal font-light mt-0'>Teacher</h4>
+          <p className='text-center font-pop text-sm leading-normal font-light mt-[12px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse feugiat nunc in leo vestibulum, at congue erat accumsan. Suspendisse potenti. Suspendisse vitae vestibulum sapien.</p>
+        </div>
+      </div>
     </div>
   )
 }
